d2499a4f0d19100211b558a8dab2d5531c90f006
Nataliya Zhuravleva <natalindria@gmail.com>
2020-10-23 10:12:06 -0700

    Add variables to UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,20 @@
 // User interface logic:
 
 $(document).ready(function () {
-
-
+  $("#formOne").submit(function(event){
+  const name = $("#name").val();
+  const age = parseInt($("#age").val());
+  const jokes = $("select#jokes").val();
+  const cartoons = $("select#cartoons").val();
+  const winMac = $("select#win_mac").val();
+  const varDef = $("select#var_def").val();
+console.log("name:"+name);
+console.log("age:"+age);
+console.log("jokes:"+jokes);
+console.log("cartoons:"+cartoons);
+console.log("winMac:"+winMac);
+console.log("varDef:"+varDef);
 
   event.preventDefault();
-
+  });
 });
